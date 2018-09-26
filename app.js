07d831e9fbe7112988880879a752ed5b83fb7c07
LuisLota <louis.miguel.05@gmail.com>
2018-09-26 16:19:16 +0100

    animation each task

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
 const app = new Vue({
 el: "#app",
 data: {
-    title: 'Todo-List ',
+    title: 'Todo App ',
     newTodo: '',
-    dayOfWeek: '',
     todos: [],
-    numberTask:0,
-    dateNow: ''
+    numberTask: 0,
+    did: false
+    
     
 },
 
 methods: {
 
     addTodo(){
-       // console.log(this.newTodo);
         this.todos.push({
-            title: this.newTodo,
-            
+            title: this.newTodo,    
             done: false
-        });
 
+        });
+        this.did = false;
         this.newTodo = '';
         this.numberTask+=1;
         
+         
     },
 
     removeTodo(todo){
         const todoIndex = this.todos.indexOf(todo);
         this.todos.splice(todoIndex,1);
+        this.numberTask < 0 ? this.numberTask = 0 :  this.numberTask-=1;
     },
 
-    allDone(){
-
-        this.todos.forEach(todo => {
-        todo.done = true;   
-        
-        });
-    },
-
-
+    remove(todo){
+        const todoIndex = this.todos.indexOf(todo);
+       this.did = true;
+    }
+  
 }
 });
